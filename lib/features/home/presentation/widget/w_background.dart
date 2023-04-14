38eb928e5ab1678c554791e08a6387638bf5b74b
mukhammadjon <muhammadjonyorqinov@gmail.com>
2023-04-14 08:57:55 -0500

    add!: details ui added

--- lib/features/home/presentation/widget/w_background.dart
+++ lib/features/home/presentation/widget/w_background.dart
@@ -4,19 +4,23 @@ import '/utils/core/dimensions.dart';
 
 class WBackground extends StatelessWidget {
   final Widget? child;
+  final VoidCallback? onPressed;
 
-  const WBackground({Key? key, this.child}) : super(key: key);
+  const WBackground({Key? key, this.child, this.onPressed}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: Dimensions.radiusAll12,
-        color: ColorName.white,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: Dimensions.radiusAll12,
+          color: ColorName.white,
+        ),
+        margin: Dimensions.paddingV8H16,
+        padding: Dimensions.paddingAll16,
+        child: child,
       ),
-      margin: Dimensions.paddingV8H16,
-      padding: Dimensions.paddingAll16,
-      child: child,
     );
   }
 }
