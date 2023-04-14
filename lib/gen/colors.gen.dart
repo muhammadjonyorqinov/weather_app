63e349bfb047d2d61a33796b1b8b02d05fcf71f3
mukhammadjon <muhammadjonyorqinov@gmail.com>
2023-04-14 02:18:48 -0500

    add!: assets added

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #171215
+  static const Color black = Color(0xFF171215);
+
+  /// Color: #2C79C1
+  static const Color blue = Color(0xFF2C79C1);
+
+  /// Color: #62B8F6
+  static const Color lightBlue = Color(0xFF62B8F6);
+
+  /// Color: #37BD6B
+  static const Color primary = Color(0xFF37BD6B);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+}
